6f4bc7566370a120771d2d319fc381b8764d0190
Sharon <Sharon@10.0.0.8>
2024-03-31 10:53:04 +0300

    add another test case for each button , in total 2 test cases for each button..

--- src/test/java/Selleniun_start_projects/Selleniun_start_projects/calculatorT.java
+++ src/test/java/Selleniun_start_projects/Selleniun_start_projects/calculatorT.java
@@ -1,15 +1,13 @@
 package Selleniun_start_projects.Selleniun_start_projects;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertEquals;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.lang.System;
 
 public class calculatorT {
@@ -27,6 +25,120 @@ public class calculatorT {
 		
 	  } 
 	 
+	 @Test
+	 public void PlusCheck() {
+		 browser.findElement(By.id("button01")).click();
+
+		 browser.findElement(By.id("buttonplus")).click();
+		 browser.findElement(By.id("button02")).click();
+		 browser.findElement(By.id("buttonequals")).click();
+
+		 String result= browser.findElement(By.id("calculated-display")).getAttribute("value").toString();
+		 String Expected= "3";
+		 assertEquals(Expected,result);
+		 browser.close();
+	 }
+	 @Test
+	 public void PlusCheck2() {
+		 browser.findElement(By.id("button06")).click();
+
+		 browser.findElement(By.id("buttonplus")).click();
+		 browser.findElement(By.id("button08")).click();
+		 browser.findElement(By.id("buttonequals")).click();
+
+		 String result= browser.findElement(By.id("calculated-display")).getAttribute("value").toString();
+		 String Expected= "14";
+		 assertEquals(Expected,result);
+		 browser.close();
+	 }
+	 
+	 @Test
+	 public void MinusCheck() {
+		 browser.findElement(By.id("button07")).click();
+		 browser.findElement(By.id("buttonminus")).click();
+		 browser.findElement(By.id("button03")).click();
+		 browser.findElement(By.id("buttonequals")).click();
+
+		 String result= browser.findElement(By.id("calculated-display")).getAttribute("value").toString();
+		 String Expected= "4";
+		 assertEquals(Expected,result);
+		 browser.close();
+	 }
+	 
+	 @Test
+	 public void MinusCheck2() {
+		 browser.findElement(By.id("button05")).click();
+		 browser.findElement(By.id("button00")).click();
+		 browser.findElement(By.id("buttonminus")).click();
+		 browser.findElement(By.id("button02")).click();
+		 browser.findElement(By.id("button05")).click();
+		 browser.findElement(By.id("buttonequals")).click();
+
+		 String result= browser.findElement(By.id("calculated-display")).getAttribute("value").toString();
+		 String Expected= "25";
+		 assertEquals(Expected,result);
+		 browser.close();
+	 }
+	 
+	 @Test
+	 public void multiCheck() {
+		 browser.findElement(By.id("button02")).click();
+		 browser.findElement(By.id("buttonmultiply")).click();
+		 browser.findElement(By.id("button01")).click();
+		 browser.findElement(By.id("button05")).click();
+		 browser.findElement(By.id("buttonequals")).click();
+
+
+		 String result= browser.findElement(By.id("calculated-display")).getAttribute("value").toString();
+		 String Expected= "30";
+		 assertEquals(Expected,result);
+		 browser.close();
+	 }
+	 
+	 @Test
+	 public void multiCheck2() {
+		 browser.findElement(By.id("button03")).click();
+		 browser.findElement(By.id("buttonmultiply")).click();
+		 browser.findElement(By.id("button04")).click();
+		 browser.findElement(By.id("buttonequals")).click();
+
+
+		 String result= browser.findElement(By.id("calculated-display")).getAttribute("value").toString();
+		 String Expected= "12";
+		 assertEquals(Expected,result);
+		 browser.close();
+	 }
+	 
+	 @Test
+	 public void divisionCheck() {
+		 browser.findElement(By.id("button03")).click();
+		 browser.findElement(By.id("button06")).click();
+		 browser.findElement(By.id("buttondivide")).click();
+		 browser.findElement(By.id("button06")).click();
+		 browser.findElement(By.id("buttonequals")).click();
+
+		 String result= browser.findElement(By.id("calculated-display")).getAttribute("value").toString();
+		 String Expected= "6";
+		 assertEquals(Expected,result);
+		 browser.close();
+	 }
+	 
+	 @Test
+	 public void divisionCheck2() {
+		 browser.findElement(By.id("button02")).click();
+		 browser.findElement(By.id("button00")).click();
+		 browser.findElement(By.id("buttondivide")).click();
+		 browser.findElement(By.id("button05")).click();
+		 browser.findElement(By.id("buttonequals")).click();
+
+		 String result= browser.findElement(By.id("calculated-display")).getAttribute("value").toString();
+		 String Expected= "4";
+		 assertEquals(Expected,result);
+		 browser.close();
+	 }
+	 
+	 
 	 
 	 
 }
+	 
